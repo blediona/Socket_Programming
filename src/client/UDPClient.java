c024aebcc381e253fe93fdd710f258972e741b5f
blediona <blediona.aliu@student.uni-pr.edu>
2026-04-14 22:31:17 +0200

    Created base structure for UDP client

--- src/client/UDPClient.java
+++ src/client/UDPClient.java
@@ -1,4 +1,27 @@
 package client;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 
 public class UDPClient {
+    public static void main(String[] args){
+        try{
+            DatagramSocket clientSocket = new DatagramSocket();
+
+            InetAddress serverAddress = InetAddress.getByName("localhost");
+            int serverPort = 9876;
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter your name: ");
+            String clientName = scanner.nextLine();
+
+            System.out.println("You can start typing messages: ");
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
