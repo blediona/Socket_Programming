fcdf6e25c4fd2138acc066f184a2e3334c02c48a
Blend-Selmani <blend.selmani3@student.uni-pr.edu>
2026-04-15 13:56:28 +0200

    pjesa e 2 e serverit
    kodi i cili vendos serverin ne dispozicon vazhdimisht per pritjen dhe perpunimin e mesazheve te klienteve

--- src/server/UDPServer.java
+++ src/server/UDPServer.java
@@ -7,7 +7,6 @@ public class UDPServer {
     public static void main(String[] args) {
         try {
             DatagramSocket serverSocket = new DatagramSocket(9876);
-            byte[] receiveData = new byte[1024];
 
             // me kete pjese te kodit arrijme ti ruajme klientet ne nje list
             HashSet<SocketAddress> clients = new HashSet<>();
@@ -16,6 +15,27 @@ public class UDPServer {
             System.out.println("Server is running...");
 
 
+            // pjesa e kodit me te cilen serveri qendron vazhdimisht i hapur dhe ne dispozicion 
+            //per te pritur mesazhe te klienteve
+            while(true){
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new
+
+                DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+
+                String message = new String(receivePacket.getData(),0, receivePacket.getLength());
+                SocketAddress clientAddress = receivePacket.getSocketAddress();
+
+                if(!clients.contains(clientAddress)){
+                    clients.add(clientAddress);
+                    System.out.println("New client connected: " + clientAddress);
+                    System.out.println("Total connected clients: " + clients.size());
+                }
+
+                System.out.println("Received from " + clientAddress + ": " + message);
+
+            }
 
 
 
